9cd49a35054b0bb2c6c780aa276777336a26f77f
ginger-kang <kdhoon07@gmail.com>
2020-05-25 20:47:10 +0900

    Use Mutation

--- graphql/db.js
+++ graphql/db.js
@@ -1,25 +1,44 @@
-export const people = [
+export let movies = [
     {
-        id: "1",
-        name: "kang",
-        age: 20,
-        gender: "male"
+        id: 1,
+        name: "LaLaLand",
+        score: 5
     },
     {
-        id: "2",
-        name: "choi",
-        age: 21,
-        gender: "female"
+        id: 2,
+        name: "Whiplash",
+        score: 5
     },
     {
-        id: "3",
-        name: "ruka",
-        age: 18,
-        gender: "male"
+        id: 3,
+        name: "MadMax",
+        score: 5
     }
 ];
 
+export const getMovies = () => movies;
+
 export const getById = id => {
-    const filteredPeople = people.filter(person => person.id == id);
-    return filteredPeople[0]
-}
+    const filteredMovies = movies.filter(movie => movie.id === id);
+    return filteredMovies[0]
+};
+
+export const deleteMovie = id => {
+    const cleanedMovies = movies.filter(movie => movie.id !== id);
+    if ( movies.length > cleanedMovies.length ) {
+        movies = cleanedMovies;
+        return true;
+    } else {
+        return false;
+    }
+};
+
+export const addMovie = (name, score) => {
+    const newMovie = {
+        id: `${movies.length + 1}`,
+        name,
+        score, 
+    };
+    movies.push(newMovie);
+    return newMovie;
+};
